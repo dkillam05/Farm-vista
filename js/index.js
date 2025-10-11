38e95d52f27c296d36cf2f0e2509c3e98cb3c40b
dkillam05 <danekillam@gmail.com>
2025-10-11 07:28:40 -0500

    Add initial app globals for menu data

---
+++ js/index.js
@@ -0,0 +1,2 @@
+// Entry point placeholder for FarmVista app (Step 1)
+console.info(`FarmVista ${window.FV_VERSION} initialized.`);
---
+++ js/ui-nav.js
@@ -0,0 +1,2 @@
+// UI navigation placeholder for FarmVista app shell.
+// Actual rendering logic will be implemented in later steps.
