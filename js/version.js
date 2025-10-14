21c24cc8db376c99bd2ee58ef8012af492175fe2
dkillam05 <danekillam@gmail.com>
2025-10-14 06:49:23 -0500

    Update version.js

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 /* FarmVista single source of truth for version + tagline */
-window.FarmVistaVersion = "1.6.3";
+window.FarmVistaVersion = "1.6.4";
 window.FV_VERSION = {
   number: "1.0.0",
   date: "2025-10-13",
