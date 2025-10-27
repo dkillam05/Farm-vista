a4cbede6a16e854371c2c50fcc31d4977a8d914c
dkillam05 <danekillam@gmail.com>
2025-10-27 07:09:33 -0500

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,17 @@
+// /Farm-vista/js/firebase-config.js
+// Defines the global Firebase config used by /js/firebase-init.js everywhere.
+
+window.FV_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB3sLBWFDsoZRLmfQ19hKLoH_nMrHEFQME",
+  authDomain: "dowsonfarms-illinois.firebaseapp.com",
+  projectId: "dowsonfarms-illinois",
+
+  // NOTE: For Firebase Storage, the SDK expects the *bucket name*,
+  // which uses appspot.com (not firebasestorage.app).
+  // Using .appspot.com avoids Storage API quirks later:
+  storageBucket: "dowsonfarms-illinois.appspot.com",
+
+  messagingSenderId: "300398089669",
+  appId: "1:300398089669:web:def2c52650a7eb67ea27ac",
+  measurementId: "G-QHBEXVGNJT"
+};
