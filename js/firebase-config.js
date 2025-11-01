284b43ae54945601ef02a22cd4dd5e5abce7064f
dkillam05 <danekillam@gmail.com>
2025-11-01 09:53:30 -0500

    Change Firebase storage bucket to new-style name
    Updated the storage bucket name to the new-style format.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,13 @@
 // /Farm-vista/js/firebase-config.js
-// Defines the global Firebase config used by /js/firebase-init.js everywhere.
+// Global Firebase config used by /js/firebase-init.js
 
 window.FV_FIREBASE_CONFIG = {
   apiKey: "AIzaSyB3sLBWFDsoZRLmfQ19hKLoH_nMrHEFQME",
   authDomain: "dowsonfarms-illinois.firebaseapp.com",
   projectId: "dowsonfarms-illinois",
 
-  // NOTE: For Firebase Storage, the SDK expects the *bucket name*,
-  // which uses appspot.com (not firebasestorage.app).
-  // Using .appspot.com avoids Storage API quirks later:
-  storageBucket: "dowsonfarms-illinois.appspot.com",
+  // IMPORTANT: This project uses the new-style bucket name.
+  storageBucket: "dowsonfarms-illinois.firebasestorage.app",
 
   messagingSenderId: "300398089669",
   appId: "1:300398089669:web:def2c52650a7eb67ea27ac",
