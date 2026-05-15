3c269ee04b1777d19db8936ba61e4d29d59407aa
dkillam05 <danekillam@gmail.com>
2026-05-15 14:38:50 -0500

    Update version.js

--- js/version.js
+++ js/version.js
@@ -12,7 +12,7 @@
 
   // ---- EDIT THESE FOR RELEASES ONLY ----
   window.FV_VERSION = {
-    number:  "05.15.07",
+    number:  "05.15.08",
     date:    "2026-05-15",
     tagline: "Farm Data - Simplified"
   };
