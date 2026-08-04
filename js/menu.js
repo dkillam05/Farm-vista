6c305096911d7df24b915e590553f062883263d7
dkillam05 <danekillam@gmail.com>
2026-08-04 06:56:04 -0500

    Comment out logistics menu href
    Comment out the href for the logistics menu item.

--- js/menu.js
+++ js/menu.js
@@ -144,7 +144,7 @@ export const NAV_MENU = {
       perm: 'logistics',
       icon: '🚛',
       label: 'Logistics',
-      href: '/Farm-vista/pages/logistics/index.html',
+      //href: '/Farm-vista/pages/logistics/index.html',
       collapsible: true,
       initialOpen: false,
       children: [
