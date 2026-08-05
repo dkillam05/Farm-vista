38022f89d09846c311c7c221749266a7e4d06d36
dkillam05 <danekillam@gmail.com>
2026-08-05 05:23:07 -0500

    Delete js/pdf.mjs

--- js/pdf.mjs
+++
@@ -1,7 +0,0 @@
-// /Farm-vista/js/pdf.mjs
-//
-// FarmVista PDF.js bridge
-// This simply re-exports the official PDF.js module.
-//
-
-export * from "https://cdn.jsdelivr.net/npm/pdfjs-dist@6.2.108/build/pdf.mjs";
