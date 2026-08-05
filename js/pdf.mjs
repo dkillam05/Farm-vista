fbdee2da7c3f3964cc7908eb20add4361d546df3
dkillam05 <danekillam@gmail.com>
2026-08-05 05:21:26 -0500

    Create pdf.mjs

---
+++ js/pdf.mjs
@@ -0,0 +1,7 @@
+// /Farm-vista/js/pdf.mjs
+//
+// FarmVista PDF.js bridge
+// This simply re-exports the official PDF.js module.
+//
+
+export * from "https://cdn.jsdelivr.net/npm/pdfjs-dist@6.2.108/build/pdf.mjs";
