802ebe8f211af3aab1d1c42489f52f3f50bd1cfe
dkillam05 <danekillam@gmail.com>
2026-09-13 20:15:07 -0500

    Load hauling job picker v7

--- js/grain-hauling-partial-checkboxes.js
+++ js/grain-hauling-partial-checkboxes.js
@@ -17,7 +17,7 @@
   if (!document.querySelector('script[data-fv-hauling-local-picker="1"]')) {
     const pickerScript = document.createElement('script');
     pickerScript.type = 'module';
-    pickerScript.src = '/js/grain-hauling-job-form-pickers-v6.js?v=20260913-2110';
+    pickerScript.src = '/js/grain-hauling-job-form-pickers-v7.js?v=20260913-2135';
     pickerScript.dataset.fvHaulingLocalPicker = '1';
     document.head.appendChild(pickerScript);
   }
