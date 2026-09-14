8ff6dcfb7fa58c33ae45f052656ece4afb63ff89
dkillam05 <danekillam@gmail.com>
2026-09-13 20:27:36 -0500

    Refresh unified hauling picker after lockup fix

--- js/grain-hauling-partial-checkboxes.js
+++ js/grain-hauling-partial-checkboxes.js
@@ -17,7 +17,7 @@
   if (!document.querySelector('script[data-fv-hauling-local-picker="1"]')) {
     const pickerScript = document.createElement('script');
     pickerScript.type = 'module';
-    pickerScript.src = '/js/grain-hauling-job-form-pickers-v9.js?v=20260913-2225';
+    pickerScript.src = '/js/grain-hauling-job-form-pickers-v9.js?v=20260913-2232';
     pickerScript.dataset.fvHaulingLocalPicker = '1';
     document.head.appendChild(pickerScript);
   }
